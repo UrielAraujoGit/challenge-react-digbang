6b119f760faeee077f8e3e1498582d12d464f302
uriel <urielaraujo@live.com.ar>
2024-04-14 19:53:36 -0300

    install tailwindcss v3

--- src/components/credit-container.tsx
+++ src/components/credit-container.tsx
@@ -31,57 +31,59 @@ const CreditContainer: FC<CreditContainerProps> = ({
 
   return (
     <>
-      <h1>Simulá tu crédito</h1>
-      <div>
-        <CreditSlider
-          min={5000}
-          max={50000}
-          title="monto total"
-          onChange={(n) => {
-            setAmount((prev) => ({ valid: prev.valid, value: n }));
-          }}
-          validityChange={(valid) => {
-            setAmount((prev) => ({ valid, value: prev.value }));
-          }}
-        />
+      <section className="bg-blue-800">
+        <h1>Simulá tu crédito</h1>
+        <div>
+          <CreditSlider
+            min={5000}
+            max={50000}
+            title="monto total"
+            onChange={(n) => {
+              setAmount((prev) => ({ valid: prev.valid, value: n }));
+            }}
+            validityChange={(valid) => {
+              setAmount((prev) => ({ valid, value: prev.value }));
+            }}
+          />
 
-        <CreditSlider
-          min={3}
-          max={24}
-          title="plazo"
-          onChange={(n) => {
-            setPayments((prev) => ({ value: n, valid: prev.valid }));
-          }}
-          validityChange={(valid) => {
-            setPayments((prev) => ({ value: prev.value, valid }));
-          }}
-        />
-      </div>
-      <div>
-        <h6>
-          Cuotas fijas por mes <span>${paymentsAmout || "-"}</span>
-        </h6>
-      </div>
-      <div>
-        <button
-          onClick={(e) => openModalCredit(amount.value, payments.value)}
-          disabled={disabledBtns}
-        >
-          Obtené crédito
-        </button>
-        <button
-          onClick={(e) =>
-            openModalPaymentsDetails(
-              amount.value,
-              payments.value,
-              Number(paymentsAmout)
-            )
-          }
-          disabled={disabledBtns}
-        >
-          Ver detalle de cuotas
-        </button>
-      </div>
+          <CreditSlider
+            min={3}
+            max={24}
+            title="plazo"
+            onChange={(n) => {
+              setPayments((prev) => ({ value: n, valid: prev.valid }));
+            }}
+            validityChange={(valid) => {
+              setPayments((prev) => ({ value: prev.value, valid }));
+            }}
+          />
+        </div>
+        <div>
+          <h6>
+            Cuotas fijas por mes <span>${paymentsAmout || "-"}</span>
+          </h6>
+        </div>
+        <div>
+          <button
+            onClick={(e) => openModalCredit(amount.value, payments.value)}
+            disabled={disabledBtns}
+          >
+            Obtené crédito
+          </button>
+          <button
+            onClick={(e) =>
+              openModalPaymentsDetails(
+                amount.value,
+                payments.value,
+                Number(paymentsAmout)
+              )
+            }
+            disabled={disabledBtns}
+          >
+            Ver detalle de cuotas
+          </button>
+        </div>
+      </section>
     </>
   );
 };
