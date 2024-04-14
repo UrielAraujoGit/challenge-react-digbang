ea164a67902be168209089ea530118cbdbd84ce3
uriel <urielaraujo@live.com.ar>
2024-04-13 22:07:15 -0300

    credit-slider-component: handle invalid input cases

--- src/components/credit-slider.tsx
+++ src/components/credit-slider.tsx
@@ -14,7 +14,10 @@ const CreditSlider: FC<CreditSliderProps> = ({
   title,
   onChange,
 }: CreditSliderProps) => {
-  const [value, setValue] = useState(0);
+  // this state handles the values of the input number  as well as the value of the slider
+  // the value of the slider will always be valid, but the value of the input could be not valid
+  const [data, setData] = useState({ input: "", value: 0 });
+  const [lastInputValidityCheck, setLastInputValidityCheck] = useState("");
 
   const formatNumber = (n: number | Array<number>) => {
     if (typeof n !== "number") {
@@ -25,11 +28,31 @@ const CreditSlider: FC<CreditSliderProps> = ({
     return n;
   };
 
-  const handleSetValue = (n: number | null) => {
+  const handleSetData = (n: number | null) => {
     if (n !== null) {
-      setValue(n);
+      setData({ input: String(n), value: n });
       onChange(n);
     }
+  };
+
+  const handleFocusOutInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const element = e.currentTarget;
+    setLastInputValidityCheck(element.value);
+
+    // if the value of the input is not valid, it shouldn't send that value to the slider 
+    if (!element.validity.valid) {
+      if (lastInputValidityCheck !== element.value) {
+        element.reportValidity();
+        setData({ ...data, value: 0 });
+      }
+      return;
+    }
+
+    handleSetData(Number(e.currentTarget.value));
+  };
+
+  const handleChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setData({ ...data, input: e.currentTarget.value });
   };
 
   return (
@@ -37,13 +60,21 @@ const CreditSlider: FC<CreditSliderProps> = ({
       <div>
         <div>
           <h6>{title}</h6>
-          <p>{value}</p>
+          <input
+            type="number"
+            min={min}
+            max={max}
+            value={data.input}
+            onBlur={handleFocusOutInput}
+            onChange={handleChangeInput}
+          />
         </div>
         <div>
           <Slider
             min={min}
             max={max}
-            onChange={(e) => handleSetValue(formatNumber(e))}
+            value={data.value}
+            onChange={(e) => handleSetData(formatNumber(e))}
           />
         </div>
       </div>
